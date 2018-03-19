bf03e3cf0d8786dae0f4b13178b0e9f9d22b7f69
Jad Georges <jad.grgs@gmail.com>
2018-03-19 12:28:21 -0700

    add etch routes

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const
     MongoDBStore = require('connect-mongodb-session')(session),
     passport = require('passport'),
     passportConfig = require('./config/passport.js'),
-    userRoutes = require('./routes/users.js')
+    userRoutes = require('./routes/users.js'),
+    etchRoutes = require('./routes/etches.js')
    
 
 
@@ -34,6 +35,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(ejsLayouts)
 app.use(cookieParser())
 app.use('/', userRoutes)
+app.use(':userId/etches', etchRoutes)
 
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'ejs')
