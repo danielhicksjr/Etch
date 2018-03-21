641bf1e22f909b1dfc09009d3e41b96a19d56326
Jad Georges <jad.grgs@gmail.com>
2018-03-21 12:15:24 -0700

    consistent user routes structure

--- routes/users.js
+++ routes/users.js
@@ -6,35 +6,34 @@ const
     User = require('../models/User.js')
  
 
-    userRoutes.route('/login')
-        .get((req, res) => {
-            res.render('users/login', { message: req.flash('loginMessage')})
-        })
-        .post(passport.authenticate('local-login', {
-            successRedirect: '/profile', 
-            failureRedirect: '/login'
-        }))
- 
-    userRoutes.route('/signup')
-        .get((req, res) => {
-                res.render('users/signup',  { message: req.flash('signupMessage')}) 
-        })
-        .post(passport.authenticate('local-signup', {
-            successRedirect: '/profile',
-            failureRedirect: '/signup'
-        }))
-
-    userRoutes.get('/profile', isLoggedIn, (req, res) => {
-        Etch.find({user: req.user}, (err, allEtches) => {
-            res.render('users/profile', {user: req.user, message: req.flash('welcomeMessage'), etches: allEtches})
-        })
+userRoutes.get('/login', (req, res) => {
+    res.render('users/login', { message: req.flash('loginMessage')})
+})
         
-    })
+userRoutes.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile', 
+    failureRedirect: '/login'
+}))
+ 
+userRoutes.get('/signup', (req, res) => {
+    res.render('users/signup',  { message: req.flash('signupMessage')}) 
+})
+        
+userRoutes.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup'
+}))
 
-    userRoutes.get('/logout', (req, res) => {
-        req.logout()
-        res.redirect('/')
-    })
+userRoutes.get('/profile', isLoggedIn, (req, res) => {
+    Etch.find({user: req.user}, (err, allEtches) => {
+        res.render('users/profile', {user: req.user, message: req.flash('welcomeMessage'), etches: allEtches})
+    })    
+})
+
+userRoutes.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 userRoutes.get('/:userId', (req, res) => {
     User.findById(req.params.userId, (err, thatUser) => {
@@ -58,9 +57,9 @@ userRoutes.delete('/:userId', (req, res) => {
     })
 })
 
-    function isLoggedIn(req, res, next){
-        if(req.isAuthenticated()) return next()
-        res.redirect('/')
-    }
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) return next()
+    res.redirect('/')
+}
 
-    module.exports = userRoutes
+module.exports = userRoutes
